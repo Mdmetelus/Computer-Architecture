035850956e3f5fea527e50b34587bbc23dcadd38
Mr. Metelus <Mdmetelus@gmail.com>
2019-05-09 15:56:10 -0400

    register value printed

--- ls8/ls8.c
+++ ls8/ls8.c
@@ -5,20 +5,32 @@
 /**
  * Main
  */
-int main(int argc, char **argv)
+// int main(int argc, char **argv)
+// {
+  // struct cpu cpu;
+// 
+  // if (argc != 2)
+  // {
+    // printf("Usage: ls8.c <filename>");
+    // exit(1);
+// }
+
+//   
+//   char *filename = argv[1];
+//   cpu_init(&cpu);
+//   // pass input to load
+//   cpu_load(&cpu, filename);
+//   cpu_run(&cpu);
+
+//   return 0;
+// }
+
+int main(void)
 {
   struct cpu cpu;
 
-  if (argc != 2)
-  {
-    printf("Usage: ls8.c <filename>");
-    exit(1);
-  }
-
-  char *filename = argv[1];
-
   cpu_init(&cpu);
-  cpu_load(&cpu, filename);
+  cpu_load(&cpu);
   cpu_run(&cpu);
 
   return 0;
